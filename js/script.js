db3f8eea10266db768e723bca94008437576c5f8
raquel_e <raqescar@gmail.com>
2022-02-22 12:16:14 -0700

    check to see if word has been guessed correctly

--- js/script.js
+++ js/script.js
@@ -1,29 +1,22 @@
 //The unordered list where the player’s guessed letters will appear.
 const liGuessedLetters = document.querySelector(".guessed-letters");
-
 //The button with the text “Guess!” in it.
 const guessButton = document.querySelector(".guess");
-
 //The text input where the player will guess a letter.
 const letterInput = document.querySelector(".letter");
-
 //The empty paragraph where the word in progress will appear.
 const wordInProgress = document.querySelector(".word-in-progress");
-
 //The paragraph where the remaining guesses will display.
 const remaining = document.querySelector(".remaining");
-
 //The span inside the paragraph where the remaining guesses will display.
 const span = document.querySelector("span");
-
 //The empty paragraph where messages will appear when the player guesses a letter.
 const message = document.querySelector(".message");
-
 //The hidden button that will appear prompting the player to play again.
 const playButton = document.querySelector(".play-again");
-
 //test word
 const word = "magnolia";
+// array for guessed letters
 const guessedLetters = [];
 
 //add symbol to represent letter in the word
@@ -54,6 +47,7 @@ guessButton.addEventListener("click", function (e) {
 
 //validate letter input 
 const validate = function (input) {
+    //check a -z 
     const acceptedLetter = /[a-zA-Z]/;
     if (input.length === 0) {
         message.innerText = "guess a letter!";
@@ -105,5 +99,21 @@ const updateWord = function (guessedLetters) {
         }
     }
     console.log(revealWord);
+    console.log(wordInProgress);
     wordInProgress.innerText = revealWord.join('');
+    winner();
 };
+
+// check if player guessed word and won 
+const winner = function () {  
+    //console.log(wordInProgress);
+    //console.log(wordInProgress.innerText);
+    if (wordInProgress.innerText === word.toUpperCase()) {
+        message.classList.add("win");
+        message.innerHTML = '<p class="highlight">You guessed correct the word! Congrats!</p>';
+    }
+};
+
+
+
+
